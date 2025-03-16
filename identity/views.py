3d2de3ddc0c47a1c11f62331eeb39fe84bc60e14
Godknows2000 <92857706+Godknows2000@users.noreply.github.com>
2025-03-16 20:40:46 +0200

    Update views.py

--- identity/views.py
+++ identity/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import auth, User
+from django.contrib import messages
+from django.http import HttpResponseRedirect, HttpResponse
+from django.urls import reverse
 
-# Create your views here.
+from identity.forms import LoginForm
+
+def login_view(request):
+    if request.method == 'POST':
+        form = LoginForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password']
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                return redirect(index_view)
+            else:
+                messages.error(request, 'Username or password is incorrect')
+        return render(request, 'identity/login.html', {'form': form})
+    else:
+        form = LoginForm()
+        return render(request, 'identity/login.html', {'form': form})
+
+def logout_view(request):
+    logout(request)
+    return redirect(reverse("identity:login"))
