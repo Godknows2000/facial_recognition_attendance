b3c7f7055e1373a50dc41d8d2b41af38d9a8add6
Godknows2000 <92857706+Godknows2000@users.noreply.github.com>
2025-03-16 20:48:47 +0200

    Update urls.py

--- facial_attendance/urls.py
+++ facial_attendance/urls.py
@@ -19,5 +19,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+        path('', include('base.urls')),
         path('identity/', include('identity.urls')),
 ]
