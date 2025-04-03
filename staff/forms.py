bd94402a9e9119067d63cd30eaa9cfed0452f93a
Godknows2000 <92857706+Godknows2000@users.noreply.github.com>
2025-04-03 09:00:03 +0200

    staff-completion

--- staff/forms.py
+++ staff/forms.py
@@ -1,38 +1,23 @@
 from django import forms
+from base.models import Staff
 from django.contrib.auth.models import User
-from base.models import Staff, Department
 
 class StaffForm(forms.ModelForm):
-    username = forms.CharField(max_length=150, required=True)
+    first_name = forms.CharField(max_length=50, required=True)
+    last_name = forms.CharField(max_length=50, required=True)
     email = forms.EmailField(required=True)
-    password = forms.CharField(widget=forms.PasswordInput, required=True)
-    first_name = forms.CharField(max_length=30, required=True)
-    last_name = forms.CharField(max_length=30, required=True)
-    
-    department = forms.ModelChoiceField(queryset=Department.objects.all(), required=True)
-    role = forms.ChoiceField(choices=Staff.ROLE_CHOICES, required=True)
+    password = forms.CharField(widget=forms.PasswordInput(), required=True)
 
     class Meta:
         model = Staff
-        fields = ['staff_id', 'department', 'role']
+        fields = '__all__'
+        exclude = ['id', 'user', 'created_at']
 
-    def save(self, commit=True):
-        # Create User object
-        user = User.objects.create_user(
-            username=self.cleaned_data['username'],
-            email=self.cleaned_data['email'],
-            password=self.cleaned_data['password'],
-            first_name=self.cleaned_data['first_name'],
-            last_name=self.cleaned_data['last_name']
-        )
-
-        # Create Staff object linked to the User
-        staff = super().save(commit=False)
-        staff.user = user
-        staff.department = self.cleaned_data['department']
-        staff.role = self.cleaned_data['role']
-
-        if commit:
-            staff.save()
+    def __init__(self, *args, **kwargs):
+        super(StaffForm, self).__init__(*args, **kwargs)
         
-        return staff
+        # Set Bootstrap form-control class for each field
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
+
+        self.fields['role'].widget.attrs['class'] = 'form-select'  # Dropdown styling
