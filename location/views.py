1575d115dee99027e7d81d5626965cb118767f16
Godknows2000 <92857706+Godknows2000@users.noreply.github.com>
2025-05-03 12:32:02 +0200

    Update views.py

--- location/views.py
+++ location/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+from core.models import Location
+from base.section import Section
+
+section = Section()
+section.actionbar = True
+section.breadcrumb = True
+
+@login_required(login_url='/identity/login')
+def index_view(request):
+    section.page_title = ""
+    section.title = "Locations"
+    section.sidebar=False
+
+    mylist = Location.objects.all().order_by('name')
+
+    context = {
+        'section': section,
+        'query_string': "",
+        'mylist': mylist,
+        
+    }
+    return render(request, 'locations/index.html', context)
